94fe1e919ca36fe92da8d992af7e8697c7b728ea
Hu <yunqing.hu@shauapp003p.rxcorp.com>
2025-06-05 07:12:02 -0400

    modified

--- api/models/rawdata.py
+++ api/models/rawdata.py
@@ -4,15 +4,71 @@ from .projects import projects
 class rawdata(models.Model):
     
     df_rawdata = models.TextField()
-    df_data_ab_others= models.TextField(null=True) #formal：null=False
-    df_mccp= models.TextField(null=True)#formal：null=False
+    df_data_ab_others= models.TextField(null=True) #formal change：null=False
+    df_mccp= models.TextField(null=True)#formal change：null=False
     brand_name= models.CharField()
     time_period_id= models.CharField()
     data_version_id= models.CharField()
-    ori_channel_list= models.TextField()
-    ori_channel_prior= models.TextField()
-    ori_segment= models.TextField()
+    ori_channel_list= models.TextField(default={
+    "f2f": 0,
+    "ht": 1,
+    "digital_gsk": 2,
+    "digital_third": 3,
+    "standalone_online": 4,
+    "collaboration_online": 5,
+    "standalone_offline": 6,
+    "collaboration_offline": 7,
+    "sponsor": 8
+    }
+)
+    ori_channel_prior= models.TextField(default={
+    "channel_name": [
+        "f2f",
+        "ht",
+        "digital_gsk",
+        "digital_third",
+        "standalone_online",
+        "collaboration_online",
+        "standalone_offline",
+        "collaboration_offline",
+        "sponsor"
+    ],
+    "channel_prior": [
+        0.56,
+        0.76,
+        0.53,
+        0.52,
+        0.68,
+        0.68,
+        0.71,
+        0.71,
+        0.75
+    ]
+}                                        )
+    ori_segment= models.TextField(default={
+    "segment_type_1": {
+        "Total_market": 0,
+        "Core": 1,
+        "Engine": 2,
+        "Others": 3
+        },
+    "segment_type_2": {
+        "Total_market": 0,
+        "Central": 1,
+        "Regional": 2,
+        "Others": 3
+        },
+    "segment_type_3": {
+        "Total_market": 0,
+        "Lupus_Center": 1, 
+        "Others": 2
+        },
+    "segment_type_4": {
+        "Total_market": 0,
+        "Lupus_Certification_Center": 1,
+        "Lupus_Demonstration_Center": 2,
+        "Others": 3
+        },
+})
     last = models.BooleanField(default=1)
     projects = models.OneToOneField(projects, on_delete=models.CASCADE)
-
-
--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,16 @@
 from django.urls import path, re_path
 
 from . import views
+
+urlpatterns = [
+      # path('test', views.test),
+    # path('groups/<str:group_name>', views.v_group, name='v_group'),
+    path('groups/', views.get_groups, name='get_groups'),
+    path('groups/<str:group_name>/', views.group_add_or_delete, name='group_add_or_delete'),
+    path('groups/<str:group_name>/rename/', views.renamegrp, name='renamegrp'),
+   
+    path('<str:group_name>/projects', views.get_projects, name='get_projects'),
+    path('projects/<str:group_name>/<str:project_name>', views.project_add_or_delete, name='project_add_or_delete'),
+   
+    path('login/', views.v_login, name='login')
+]
